e2dac72d30f0a34c449320947d2526114463d817
Dwight Ledet <dwightledet@protonmail.com>
2024-03-06 09:35:14 +0100

    check

--- GeoSteam/settings.py
+++ GeoSteam/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-x!v8i1se()p*kofad4#cu@_auml#al0#zf=j#g=vqg*v75as4g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
